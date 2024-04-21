d769ea66921a1afb746507a03424c3d07d60a426
Aryan Srivastava <aryans6204@gmail.com>
2024-04-21 17:08:56 +0530

    added google provider

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,4 +1,5 @@
 import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
@@ -6,6 +7,10 @@ export const authOptions = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
